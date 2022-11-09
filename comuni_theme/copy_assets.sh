4d7d9ae4b6d5b568d249d64f47cb9b9715d297ac
Andrea Stagi <stagi.andrea@gmail.com>
2022-11-09 12:51:12 +0100

    chore: update bsi dependency

--- comuni_theme/copy_assets.sh
+++ comuni_theme/copy_assets.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cp ./node_modules/bootstrap-italia/dist/css/bootstrap-italia-comuni.min.css ./assets/css/bootstrap-italia.min.css
-cp ./node_modules/bootstrap-italia/dist/js/bootstrap-italia.bundle.min.js ./assets/js/bootstrap-italia.bundle.min.js
+cp ./node_modules/bootstrap-italia/dist/css/bootstrap-italia-comuni.min.css ./css/bootstrap-italia.min.css
+cp ./node_modules/bootstrap-italia/dist/js/bootstrap-italia.bundle.min.js ./js/bootstrap-italia.bundle.min.js
